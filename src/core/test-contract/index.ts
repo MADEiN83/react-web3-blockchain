781c9f5af9ec8f00abbcf0c978be2357df36c6ac
Anthony DI STEFANO <Anthony.Distefano@hotmail.fr>
2022-04-22 11:11:02 +0200

    feat: use Contract address from metadata

--- src/core/test-contract/index.ts
+++ src/core/test-contract/index.ts
@@ -3,15 +3,16 @@ import web3 from "../web3";
 import json from "./definition.json";
 
 class TestContract {
-  private address = "0x5ED3c0DD9DE5EFB2C042c42dd35604a1B10a554D";
   private instance: any;
   private fromAddress: string = "";
 
   constructor() {
-    this.instance = new web3.eth.Contract(json.abi as any, this.address);
+    this.instance = new web3.eth.Contract(
+      json.abi as any,
+      json.networks[5777].address
+    );
 
     web3.eth.getAccounts().then((addresses) => {
-      console.log("Setting up address", addresses[0]);
       this.fromAddress = addresses[0];
     });
   }
@@ -51,7 +52,6 @@ class TestContract {
     });
   }
 
-  // StringHasChanged
   listen = (eventName: string, callback: (event: Event) => void) => {
     const options = {
       fromBlock: 0,
